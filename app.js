139b459c888a5365bca170070a3ff672698f0181
codebeast01 <akp998rahul@gmail.com>
2019-09-23 01:14:48 +0530

    https secure connection

--- app.js
+++ app.js
@@ -30,6 +30,13 @@ connect.then((db)=>{
 .catch((err)=> console.log(err));
 
 var app = express();
+app.all('*', (req, res, next)=>{
+	if(req.secure)
+		next();
+	else{
+		res.redirect(307, 'https://'+req.hostname+":"+app.get('secPort')+req.url);
+	}
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
